eb84a9e05bc3c3463d8a032e5f714ae1689bfd84
Jesshwanth S. Joshua <34447801+jesshwanthjoshua@users.noreply.github.com>
2024-05-08 19:35:53 +0530

    Update Password_generator.py

--- Password_generator.py
+++ Password_generator.py
@@ -4,38 +4,40 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like in your password?\n")) 
+nr_letters= int(input("How many letters would you like in your password?\n")) 
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-#Eazy Level
-# password = ""
-# for char in range(1, nr_letters + 1):
-#   password += random.choice(letters)
-# for char in range(1, nr_symbols + 1):
-#   password += random.choice(symbols)
-# for char in range(1, nr_numbers + 1):
-#   password += random.choice(numbers)
-# print(password)
-#Hard Level
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
+
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+
+password = ""
 password_list = []
 
-for char in range(1, nr_letters + 1):
-  password_list.append(random.choice(letters))
+for n in range (1, nr_letters+1):
+  num = random.randint(0,len(letters)-1)
+  password_list.append(letters[num])
+  # password_list.append(random.choice(letters))
 
-for char in range(1, nr_symbols + 1):
-  password_list += random.choice(symbols)
+for n in range (1, nr_symbols+1):
+  num = random.randint(0,len(symbols)-1)
+  password_list.append(symbols[num])
+  # password_list.append(random.choice(symbols))
 
-for char in range(1, nr_numbers + 1):
-  password_list += random.choice(numbers)
+for n in range (1, nr_numbers+1):
+  num = random.randint(0,len(numbers)-1)
+  password_list.append(numbers[num])
+  # password_list.append(random.choice(numbers))
 
 print(password_list)
 random.shuffle(password_list)
 print(password_list)
 
-password = ""
-for char in password_list:
-  password += char
-
-print(f"Your password is: {password}")
+for X in password_list:
+  password += X
+  
+print(f"Your new generated password is {password}")
